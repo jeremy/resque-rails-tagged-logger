9394306a8a1ee0d6cc5c87e278024a6e2d8d460f
Jeremy Kemper <jeremy@bitsweat.net>
2012-12-06 13:05:08 -0700

    Override the log_tags method to provide your own tags

--- lib/resque/plugins/rails_tagged_logger.rb
+++ lib/resque/plugins/rails_tagged_logger.rb
@@ -2,7 +2,18 @@ module Resque
   module Plugins
     module RailsTaggedLogger
       def around_perform_rails_tagged_logger(*args, &block)
-        Rails.logger.tagged :resque, name, &block
+        Rails.logger.tagged log_tags, &block
+      end
+
+      # Override to provide your own tags. Takes the same arguments as your
+      # perform method. Defaults to [:resque, ClassName].
+      #
+      # Example:
+      #   def log_tags(record_id)
+      #     super + [record_id]
+      #   end
+      def log_tags(*args)
+        [:resque, name]
       end
     end
   end
